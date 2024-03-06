5cdc37af087c9c930d49faa51117f65297f1e7df
Kevin Alavik <kevin@alavik.se>
2024-03-06 18:58:47 +0000

    Final commit for today, bye :=)

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -9,6 +9,7 @@
 
 void main(struct multiboot_info* mb_info)
 {
+	dprintf("\033c");
 	dprintf("Hello from Sinx v0.0.1\n");
 	dprintf("Bootloader: %s\n", (char*)mb_info->boot_loader_name);
 	dprintf("%ux%u\n", mb_info->framebuffer_width, mb_info->framebuffer_height);
